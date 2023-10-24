f85952c9e75b7e39318aa076dd7362744bcc27db
aayushker <singhaayushker@gmail.com>
2023-10-24 13:36:14 +0530

    Added AQI API

---
+++ script2.js
@@ -0,0 +1,20 @@
+const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=Seattle';
+const option = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cf372d5e48mshc3464f2d07a949dp15564fjsn1d50fdc6fd23',
+		'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+async function fetchData() {
+    try {
+      const response = await fetch(url, option);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  fetchData();
